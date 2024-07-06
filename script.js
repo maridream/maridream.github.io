93e5bec04178b4a71ac809c02adb1d1f8f07d3d7
maridream <sterlingx13@gmail.com>
2024-07-05 23:57:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const bgAudio = document.getElementById('bgAudio');
-
-bgAudio.volume = 0.2;
-bgAudio.addEventListener('ended', function() {
-  this.currentTime = 0;
-  this.play();
-}, false);
-
-
-
 function showContent(contentId) {
   var contents = document.querySelectorAll('.hide');
   contents.forEach(function(content) {
